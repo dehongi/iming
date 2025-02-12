5c7c1679012e7408c63c7a99099bd6273258c5f2
Hamed Mohammadi <hamed@dehongi.com>
2025-02-12 11:33:00 +0330

    iming app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """
+    Custom user model that inherits from Django's AbstractUser .
+    """
+
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    bio = models.TextField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
---
+++ iming/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ImingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'iming'
