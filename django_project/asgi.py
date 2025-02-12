d0cc61810e1c19f60e2c17f78b51551fca813213
Hamed Mohammadi <hamed@dehongi.com>
2025-02-12 15:40:42 +0400

    settings

--- django_project/asgi.py
+++ django_project/asgi.py
@@ -8,23 +8,21 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from django_project.urls import websocket_urlpatterns  # Importing websocket_urlpatterns
+from channels.routing import ProtocolTypeRouter, URLRouter
 
+# Set the Django settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
 
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
+# Initialize Django ASGI application early to ensure the AppRegistry is populated
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-})
+# Import your websocket_urlpatterns after Django is set up
+from django_project.urls import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": URLRouter(websocket_urlpatterns),
+    }
+)
--- django_project/settings.py
+++ django_project/settings.py
@@ -128,3 +128,13 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+# Channel layer settings
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
